25c83b9dd608e56215f030f71821cc194affa870
Georgios Charatsaris <gxaratsaris@gmail.com>
2021-02-09 11:18:58 +0100

    lda.py

--- models/lda.py
+++ models/lda.py
@@ -12,7 +12,7 @@ parser.add_argument('--vectorizer', type=str, default='cv', help='the CountVecto
 parser.add_argument('--min_df', type=int, default=2, help='the minimum number of documents containing a word')
 parser.add_argument('--max_df', type=float, default=0.7, help='the maximum number of topics containing a word')
 parser.add_argument('--size', type=int, default=100, help='the size of the w2v embeddings')
-parser.add_argument('--num_topics', type=int, default=20, help='the number of topics')
+parser.add_argument('--num_topics', type=int, default=2, help='the number of topics')
 parser.add_argument('--top_words', type=int, default=10, help='the number of top words for each topic')
 opt = parser.parse_args()
 
@@ -47,14 +47,18 @@ def LDA(dtm, cv, num_topics, top_words):
 
 # Save the resulted list of lists of words for each topic setting
 	df = pd.DataFrame(np.array(topic_list).T, columns=[f'Topic {i + 1}' for i in range(num_topics)])
-	df.to_excel(f'LDA_{num_topics}.xlsx')
+	df.to_excel(f'C:/Users/gxara/Downloads/LDA_{num_topics}.xlsx')
 
-	return topic_list
+# Generate the topic for each document
+	doc_top_matrix = lda.transform(dtm)
+	doc_topic_list = doc_top_matrix.argmax(axis=1)
+
+	return topic_list, doc_topic_list
 
 
 if __name__ == '__main__':
 # Define the dataset and the arguments
-	df = pd.read_csv('HeinOnline.csv')
+	df = pd.read_csv('C:/Users/gxara/Downloads/HeinOnline/HeinOnline.csv')
 	articles = df['content']
 
 # Generate the document term matrix and the vectorizer
@@ -64,10 +68,13 @@ if __name__ == '__main__':
 	bow, dictionary, w2v = get_dictionary(cv, articles, opt.min_df, opt.size)
 
 # Create the list of lists of the top words of each topic
-	topic_list = LDA(dtm, cv, opt.num_topics, opt.top_words)
+	topic_list, doc_topic_list = LDA(dtm, cv, opt.num_topics, opt.top_words)
+# Print the title of the document and its topic based on the LDA
+	df['Topic'] = doc_topic_list
+	print(df[['title', 'Topic']])
 
 # Calculate the coherence scores
-	evaluation_model = CoherenceScores(topic_list, bow, dictionary, w2v)
-	coherence_scores = evaluation_model.get_coherence_scores()
+ 	evaluation_model = CoherenceScores(topic_list, bow, dictionary, w2v)
+ 	coherence_scores = evaluation_model.get_coherence_scores()
 # Print the coherence scores C_V, NPMI, UCI, and C_W2V, respectively
-	print(coherence_scores)
+ 	print(coherence_scores)
